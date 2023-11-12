b65cdb5a02372be39100b95a4c3fe50567373d70
oussamahamani <oussamahamani2000@gmail.com>
2023-11-11 22:43:11 -0500

    copying the code from the kana project

---
+++ script.js
@@ -0,0 +1,401 @@
+var kana = {
+	'hsingle' : 
+		{ 'ã‚': 'a', 'ã„': 'i', 'ã†': 'u', 'ãˆ': 'e', 'ãŠ': 'o'},
+	'hk':
+		{ 'ã‹': 'ka', 'ã': 'ki', 'ã': 'ku', 'ã‘': 'ke', 'ã“': 'ko'},
+	'hs':
+		{ 'ã•': 'sa', 'ã—': 'shi', 'ã™': 'su', 'ã›': 'se', 'ã': 'so'},
+	'ht':
+		{ 'ãŸ': 'ta', 'ã¡': 'chi', 'ã¤': 'tsu', 'ã¦': 'te', 'ã¨': 'to'},
+	'hn':
+		{ 'ãª': 'na', 'ã«': 'ni', 'ã¬': 'nu', 'ã­': 'ne', 'ã®': 'no'},
+	'hh':
+		{ 'ã¯': 'ha', 'ã²': 'hi', 'ãµ': 'fu', 'ã¸': 'he', 'ã»': 'ho'},
+	'hm':
+		{ 'ã¾': 'ma', 'ã¿': 'mi', 'ã‚€': 'mu', 'ã‚': 'me', 'ã‚‚': 'mo'},
+	'hy':
+		{ 'ã‚„': 'ya', 'ã‚†': 'yu', 'ã‚ˆ': 'yo'},
+	'hr':
+		{ 'ã‚‰': 'ra', 'ã‚Š': 'ri', 'ã‚‹': 'ru', 'ã‚Œ': 're', 'ã‚': 'ro'},
+	'hw':
+		{ 'ã‚': 'wa', 'ã‚’': 'o'},
+	'hn1':
+		{ 'ã‚“': 'n'},
+	'hg':
+		{ 'ãŒ': 'ga', 'ãŽ': 'gi', 'ã': 'gu', 'ã’': 'ge', 'ã”': 'go'},
+	'hz':
+		{ 'ã–': 'za', 'ã˜': 'ji', 'ãš': 'zu', 'ãœ': 'ze', 'ãž': 'zo'},
+	'hd':
+		{ 'ã ': 'da', 'ã¢': 'ji', 'ã¥': 'zu', 'ã§': 'de', 'ã©': 'do'},
+	'hb':
+		{ 'ã°': 'ba', 'ã³': 'bi', 'ã¶': 'bu', 'ã¹': 'be', 'ã¼': 'bo'},
+	'hp':
+		{ 'ã±': 'pa', 'ã´': 'pi', 'ã·': 'pu', 'ãº': 'pe', 'ã½': 'po'},
+		
+	'hdk':
+		{ 'ãã‚ƒ': 'kya', 'ãã‚…': 'kyu', 'ãã‚‡': 'kyo'},
+	'hds':
+		{ 'ã—ã‚ƒ': 'sha', 'ã—ã‚…': 'shu', 'ã—ã‚‡': 'sho'},
+	'hdc':
+		{ 'ã¡ã‚ƒ': 'cha', 'ã¡ã‚…': 'chu', 'ã¡ã‚‡': 'cho'},
+	'hdn':
+		{ 'ã«ã‚ƒ': 'nya', 'ã«ã‚…': 'nyu', 'ã«ã‚‡': 'nyo'},
+	'hdh':
+		{ 'ã²ã‚ƒ': 'hya', 'ã²ã‚…': 'hyu', 'ã²ã‚‡': 'hyo'},
+	'hdm':
+		{ 'ã¿ã‚ƒ': 'mya', 'ã¿ã‚…': 'myu', 'ã¿ã‚‡': 'myo'},
+	'hdr':
+		{ 'ã‚Šã‚ƒ': 'rya', 'ã‚Šã‚…': 'ryu', 'ã‚Šã‚‡': 'ryo'},
+	'hdg':
+		{ 'ãŽã‚ƒ': 'gya', 'ãŽã‚…': 'gyu', 'ãŽã‚‡': 'gyo'},
+	'hdj':
+		{ 'ã˜ã‚ƒ': 'ja', 'ã˜ã‚…': 'ju', 'ã˜ã‚‡': 'jo'},
+	'hdj2':
+		{ 'ã¢ã‚ƒ': 'ja', 'ã¢ã‚…': 'ju', 'ã¢ã‚‡': 'jo'},
+	'hdb':
+		{ 'ã³ã‚ƒ': 'bya', 'ã³ã‚…': 'byu', 'ã³ã‚‡': 'byo'},
+	'hdp':
+		{ 'ã´ã‚ƒ': 'pya', 'ã´ã‚…': 'pyu', 'ã´ã‚‡': 'pyo'},
+	
+		
+	'ksingle' : 
+		{ 'ã‚¢': 'a', 'ã‚¤': 'i', 'ã‚¦': 'u', 'ã‚¨': 'e', 'ã‚ª': 'o'},
+	'kk':
+		{ 'ã‚«': 'ka', 'ã‚­': 'ki', 'ã‚¯': 'ku', 'ã‚±': 'ke', 'ã‚³': 'ko'},
+	'ks':
+		{ 'ã‚µ': 'sa', 'ã‚·': 'shi', 'ã‚¹': 'su', 'ã‚»': 'se', 'ã‚½': 'so'},
+	'kt':
+		{ 'ã‚¿': 'ta', 'ãƒ': 'chi', 'ãƒ„': 'tsu', 'ãƒ†': 'te', 'ãƒˆ': 'to'},
+	'kn':
+		{ 'ãƒŠ': 'na', 'ãƒ‹': 'ni', 'ãƒŒ': 'nu', 'ãƒ': 'ne', 'ãƒŽ': 'no'},
+	'kh':
+		{ 'ãƒ': 'ha', 'ãƒ’': 'hi', 'ãƒ•': 'fu', 'ãƒ˜': 'he', 'ãƒ›': 'ho'},
+	'km':
+		{ 'ãƒž': 'ma', 'ãƒŸ': 'mi', 'ãƒ ': 'mu', 'ãƒ¡': 'me', 'ãƒ¢': 'mo'},
+	'ky':
+		{ 'ãƒ¤': 'ya', 'ãƒ¦': 'yu', 'ãƒ¨': 'yo'},
+	'kr':
+		{ 'ãƒ©': 'ra', 'ãƒª': 'ri', 'ãƒ«': 'ru', 'ãƒ¬': 're', 'ãƒ­': 'ro'},
+	'kw':
+		{ 'ãƒ¯': 'wa', 'ãƒ²': 'o'},
+	'kn1':
+		{ 'ãƒ³': 'n'},
+	'kg':
+		{ 'ã‚¬': 'ga', 'ã‚®': 'gi', 'ã‚°': 'gu', 'ã‚²': 'ge', 'ã‚´': 'go'},
+	'kz':
+		{ 'ã‚¶': 'za', 'ã‚¸': 'ji', 'ã‚º': 'zu', 'ã‚¼': 'ze', 'ã‚¾': 'zo'},
+	'kd':
+		{ 'ãƒ€': 'da', 'ãƒ‚': 'ji', 'ãƒ…': 'zu', 'ãƒ‡': 'de', 'ãƒ‰': 'do'},
+	'kb':
+		{ 'ãƒ': 'ba', 'ãƒ“': 'bi', 'ãƒ–': 'bu', 'ãƒ™': 'be', 'ãƒœ': 'bo'},
+	'kp':
+		{ 'ãƒ‘': 'pa', 'ãƒ”': 'pi', 'ãƒ—': 'pu', 'ãƒš': 'pe', 'ãƒ': 'po'},
+		
+	'kdk':
+		{ 'ã‚­ãƒ£': 'kya', 'ã‚­ãƒ¥': 'kyu', 'ã‚­ãƒ§': 'kyo'},
+	'kds':
+		{ 'ã‚·ãƒ£': 'sha', 'ã‚·ãƒ¥': 'shu', 'ã‚·ãƒ§': 'sho'},
+	'kdc':
+		{ 'ãƒãƒ£': 'cha', 'ãƒãƒ¥': 'chu', 'ãƒãƒ§': 'cho'},
+	'kdn':
+		{ 'ãƒ‹ãƒ£': 'nya', 'ãƒ‹ãƒ¥': 'nyu', 'ãƒ‹ãƒ§': 'nyo'},
+	'kdh':
+		{ 'ãƒ’ãƒ£': 'hya', 'ãƒ’ãƒ¥': 'hyu', 'ãƒ’ãƒ§': 'hyo'},
+	'kdm':
+		{ 'ãƒŸãƒ£': 'mya', 'ãƒŸãƒ¥': 'myu', 'ãƒŸãƒ§': 'myo'},
+	'kdr':
+		{ 'ãƒªãƒ£': 'rya', 'ãƒªãƒ¥': 'ryu', 'ãƒªãƒ§': 'ryo'},
+	'kdg':
+		{ 'ã‚®ãƒ£': 'gya', 'ã‚®ãƒ¥': 'gyu', 'ã‚®ãƒ§': 'gyo'},
+	'kdj':
+		{ 'ã‚¸ãƒ£': 'ja', 'ã‚¸ãƒ¥': 'ju', 'ã‚¸ãƒ§': 'jo'},
+	'kdj2':
+		{ 'ãƒ‚ãƒ£': 'ja', 'ãƒ‚ãƒ¥': 'ju', 'ãƒ‚ãƒ§': 'jo'},
+	'kdb':
+		{ 'ãƒ“ãƒ£': 'bya', 'ãƒ“ãƒ¥': 'byu', 'ãƒ“ãƒ§': 'byo'},
+	'kdp':
+		{ 'ãƒ”ãƒ£': 'pya', 'ãƒ”ãƒ¥': 'pyu', 'ãƒ”ãƒ§': 'pyo'},
+}
+
+var show_tools = ['ã‚', 'ã„', 'ã†', 'ãˆ', 'ãŠ', 'ã‹', 'ã', 'ã', 'ã‘', 'ã“', 'ã•', 'ã—', 'ã™', 'ã›', 'ã', 'ãŸ', 'ã¡', 'ã¤', 'ã¦', 'ã¨', 'ãª', 'ã«', 'ã¬', 'ã­', 'ã®', 'ã¯', 'ã²', 'ãµ', 'ã¸', 'ã»', 'ã¾', 'ã¿', 'ã‚€', 'ã‚', 'ã‚‚', 'ã‚„', 'ã‚†', 'ã‚ˆ', 'ã‚‰', 'ã‚Š', 'ã‚‹', 'ã‚Œ', 'ã‚', 'ã‚', 'ã‚’', 'ã‚“', 'ã‚¢', 'ã‚¤', 'ã‚¦', 'ã‚¨', 'ã‚ª', 'ã‚«', 'ã‚­', 'ã‚¯', 'ã‚±', 'ã‚³', 'ã‚µ', 'ã‚·', 'ã‚¹', 'ã‚»', 'ã‚½', 'ã‚¿', 'ãƒ', 'ãƒ„', 'ãƒ†', 'ãƒˆ', 'ãƒŠ', 'ãƒ‹', 'ãƒŒ', 'ãƒ', 'ãƒŽ', 'ãƒ', 'ãƒ’', 'ãƒ•', 'ãƒ˜', 'ãƒ›', 'ãƒž', 'ãƒŸ', 'ãƒ ', 'ãƒ¡', 'ãƒ¢', 'ãƒ¤', 'ãƒ¦', 'ãƒ¨', 'ãƒ©', 'ãƒª', 'ãƒ«', 'ãƒ¬', 'ãƒ­', 'ãƒ¯', 'ãƒ²', 'ãƒ³'];
+
+var replacements = {
+	'o': ['wo'],
+	'chi': ['ci'],
+	'shi': ['si'],
+	'tsu': ['tu'],
+	'zu': ['du'],
+	'ji': ['di', 'zi'],
+	'fu': ['hu'],
+	'ja': ['dya'],
+	'jo': ['dyo'],
+	'ju': ['dyu']
+};
+
+var active = [];
+var shuffled = [];
+var fonts = [];
+
+var cur_kana;
+var cur_reading;
+
+var total_answered = 0;
+var total_correct = 0;
+
+function save_settings() {
+	inputs = document.getElementsByTagName('input');
+	for (i = 0; i < inputs.length; i++) {
+		if (inputs[i].type == 'checkbox') {
+			checked = inputs[i].checked ? '1' : '0';
+			localStorage.setItem('kana_' + inputs[i].id, checked);
+		}
+	}
+	
+	collect();
+}
+
+function load_settings() {
+	inputs = document.getElementsByTagName('input');
+	for (i = 0; i < inputs.length; i++) {
+		if (inputs[i].type == 'checkbox') {
+			var setting = localStorage.getItem('kana_' + inputs[i].id);
+			if(setting === '1') {
+				inputs[i].checked = true;
+			} else if(setting === '0') {
+				inputs[i].checked = false;
+			}
+		}
+	}
+	
+	collect();
+}
+
+function check(set) {
+	var trs = document.getElementsByClassName(set);
+	for (i = 0; i < trs.length; i++) {
+		var tds = trs[i].children;
+		for (x = 0; x < tds.length; x++) {
+			if(tds[x].children[0].id != 'KOI-WIN') {
+				tds[x].children[0].checked = true;
+			}
+		}
+	}
+	save_settings();
+}
+
+function uncheck(set) {
+	var trs = document.getElementsByClassName(set);
+	for (i = 0; i < trs.length; i++) {
+		var tds = trs[i].children;
+		for (x = 0; x < tds.length; x++) {
+			tds[x].children[0].checked = false;
+		}
+	}
+	save_settings();
+}
+
+function shuffle(orig_array) {
+	var array = orig_array.slice(0);
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	while (0 !== currentIndex) {
+
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+function collect() {
+	kanacheck = document.getElementsByClassName('kanacheck');
+	active = [];
+	shuffled = [];
+	for (i = 0; i < kanacheck.length; i++) {
+		cur = kanacheck[i];
+		if(cur.checked == true) {
+			for (p in kana[cur.id]) {
+				active.push( [p, kana[cur.id][p]] );
+			}
+		}
+	}
+	
+	fonts = [];
+	fontcheck = document.getElementsByClassName('fontcheck');
+	for (i = 0; i < fontcheck.length; i++) {
+		if(fontcheck[i].checked == true) {
+			fonts.push(fontcheck[i].id);
+		}
+	}
+}
+
+function show_kana() {	
+	wrong = false;
+	document.getElementById('input_box').value = '';
+	
+	if(active.length == 0) {
+		document.getElementById('hsingle').checked = true;
+		save_settings();
+	}
+	
+	if(fonts.length == 0) {
+		document.getElementById('default').checked = true;
+		save_settings();
+	}
+	
+	if(total_answered > 0) {
+		document.getElementById('count').innerHTML = total_correct + '/' + total_answered;
+		document.getElementById('message').innerHTML = '&nbsp;';
+	}
+	
+	if(shuffled.length == 0) {
+		shuffled = shuffle(active);
+	}
+	
+	if(cur_kana && shuffled[0][0] == cur_kana) {
+		shuffled.shift();
+	}
+	
+	cur_kana = shuffled[0][0];
+	cur_reading = shuffled[0][1];
+	
+	shuffled.shift();
+	
+	var font = fonts[Math.floor(Math.random()*fonts.length)];
+	
+	if(font == 'default') {
+		document.getElementById('kana').innerHTML = cur_kana;
+	} else {
+		document.getElementById('kana').innerHTML = '<img src="fonts/' + font + '/' + cur_kana + '.png" />';
+	}
+	document.getElementById('answer').innerHTML = cur_reading;
+	
+	if(show_tools.indexOf(cur_kana) == -1) {
+		document.getElementById('tool_stroke').style.visibility = 'hidden';
+	} else {
+		document.getElementById('tool_stroke').style.visibility = 'visible';
+	}
+}
+
+function check_answer() {
+	answer = document.getElementById('input_box').value.toLowerCase();
+	if(! answer) {
+		answer = 'x';
+	}
+	
+	chars = answer.split('');
+	
+	possible = [cur_reading];
+	if(cur_reading in replacements) {
+		possible = possible.concat(replacements[cur_reading]);
+	}
+
+	for (i = 0; i < chars.length; i++) {
+		var err = true;
+		
+		for (x = 0; x < possible.length; x++) {
+			if(chars[i] == possible[x].charAt(i)) {
+				err = false;
+			}
+			if(answer == possible[x]) {
+				answer = cur_reading;
+			}
+		}
+		
+		if(err) {
+			break;
+		}
+	}
+	
+	if(err) {
+		wrong = true;
+		document.getElementById('message').innerHTML = '<span id="wrong">' + cur_kana + ' = ' + cur_reading + '</span>';
+	}
+	
+	if(answer == cur_reading) {
+		total_answered += 1;
+		if( ! wrong) {
+			total_correct += 1;
+		}
+		show_kana();
+	}
+}
+
+function force_next() {
+	if(shuffled.length > 3) {
+		shuffled.splice(3, 0, [cur_kana, cur_reading]);
+	}
+	if(shuffled.length > 13) {
+		shuffled.splice(13, 0, [cur_kana, cur_reading]);
+	}
+	total_answered += 1;
+	
+	show_kana();
+}
+
+function show_answer() {
+	document.getElementById('answer').style.visibility = 'visible'; 
+}
+
+function hide_answer() {
+	document.getElementById('answer').style.visibility = 'hidden';
+}
+
+function play_sound() {
+	var audio = new Audio('audio/' + cur_reading + '.mp3');
+	audio.play();
+	document.getElementById('input_box').focus();
+}
+
+function play_other(file) {
+	var audio = new Audio('audio/' + file + '.mp3');
+	audio.play();
+}
+
+function stroke_order() {
+	document.getElementById('kana').innerHTML = '<img src="stroke/' + cur_kana + '.gif" id="stroke" />';
+	document.getElementById('input_box').focus();
+}
+
+
+onload = function () {
+	load_settings();
+	
+	inputs = document.getElementsByTagName('input');
+	for (i = 0; i < inputs.length; i++) {
+		if (inputs[i].type == 'checkbox') {
+			inputs[i].onclick = save_settings;
+			inputs[i].onpropertychange = inputs[i].oninput;
+		}
+	}
+	
+	show_kana();
+	
+	document.getElementById('tool_sound').onclick = play_sound;
+	document.getElementById('tool_stroke').onclick = stroke_order;
+	
+	var kana_div = document.getElementById('kana');
+	kana_div.onmouseover = show_answer;
+	kana_div.onmouseout = hide_answer;
+	
+	var answer_input = document.getElementById('input_box');
+	answer_input.focus();
+	answer_input.oninput = check_answer;
+	answer_input.onpropertychange = answer_input.oninput;
+	
+	document.body.onkeydown = function(e){
+		document.getElementById('input_box').focus();
+		if(e.keyCode == 32 || e.keyCode == 13){
+			e.preventDefault();
+			if( ! wrong) {
+				check_answer();
+			} else {
+				force_next();
+			}
+		}
+	}
+}
